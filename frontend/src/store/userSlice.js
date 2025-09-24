5760f4e24faf50fb913de0cc51ff799cd053cee8
aditi14534 <aditi30102003@gmail.com>
2025-09-24 21:54:36 +0530

    made changes in userSlice and header logout

--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -11,10 +11,33 @@ export const userSlice = createSlice({
     setUserDetails: (state, action) => {
       state.user = action.payload;
     },
+    logoutUser: (state) => {
+      state.user = null;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setUserDetails } = userSlice.actions;
+export const { setUserDetails, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   user: null,
+// };
+
+// export const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     setUserDetails: (state, action) => {
+//       state.user = action.payload;
+//     },
+//   },
+// });
+
+// // Action creators are generated for each case reducer function
+// export const { setUserDetails } = userSlice.actions;
+
+// export default userSlice.reducer;
